2b45536d805e61bbda976241f5b27d5d635fb4f4
Trang <nguyenminhtrang9213@gmail.com>
2026-03-02 15:32:20 +0100

    add Grid for hero section

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,5 +1,35 @@
+import { Bolt, Dumbbell, Lock, Weight } from "lucide-react";
+import { workoutProgram as traning_plan } from "../utils/plan_db";
+
 function Grid() {
-  return <div>grid</div>;
+  const isLocked = false;
+  return (
+    <div>
+      {Object.keys(traning_plan).map((workout, workoutIndex) => {
+        return (
+          <button className="px-4 py-2 m-1 border-2 rounded-sm">
+            <div>
+              <p>
+                Day{" "}
+                {workoutIndex + 1 < 10
+                  ? "0" + (workoutIndex + 1)
+                  : workoutIndex + 1}
+              </p>
+            </div>
+            {isLocked ? (
+              <Lock />
+            ) : workoutIndex % 3 === 0 ? (
+              <Dumbbell />
+            ) : workoutIndex % 3 === 1 ? (
+              <Weight />
+            ) : (
+              <Bolt />
+            )}
+          </button>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Grid;
